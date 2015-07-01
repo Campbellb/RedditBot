a3e54bd0644b7b66cff26f779ea564b41141b771
Campbell Belden <Campbellb@gmail.com>
2015-06-30 17:17:50 -0700

    trying to get things working

--- reddit.rb
+++ reddit.rb
@@ -5,12 +5,28 @@ dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'pp'
 require 'httparty'
 
+
 class Reddit
   include HTTParty
   base_uri "https://www.reddit.com/r"
+  def initialize(subreddit, title)
+    @subreddit = subreddit
+    @title = title
+  end
 
-  def get_subreddit(subreddit)
-    self.class.get("/#{subreddit}.json")
+  def get_subreddit
+    self.class.get("/#{@subreddit}.json")
+  end
+
+  def get_comments
+    response = JSON.parse(self.get_subreddit.to_json)
+    @ids = []
+    response['data']['children'].each do |post|
+      if post['data']['title'].include? @title
+        @ids << post['data']['id']
+      end
+    end
+    ap @ids
   end
 
   def login(username, password)
@@ -19,18 +35,31 @@ class Reddit
   end
 
   def post_reply
+    options = {:api_type => "json",:text => "TESTTTT!!!!!", :thing_id => @ids.first}
+    if get_comments
+      self.class.post("/api/comment/#{@ids.first}", options)
+    end
   end
+
+  # def logged_in?
+  #   ap self.class.get("/api/v1/me")
+  # end
+
 end
 
-x = Reddit.new
-x.login('dbcbot', 'dbcbot1')
-response = x.get_subreddit("DBCTest")
-parsed = JSON.parse(response.to_json)
-ids = []
-parsed['data']['children'].each do |thingy|
-  if thingy['data']['title'].include? "dbctestpost"
-    ids << thingy['data']['id']
-  end
-end
+a = Reddit.new("dbctest", "dbctestpost")
+a.login("dbcbot", "dbcbot1")
+a.get_comments
+a.post_reply
+# x = Reddit.new
+# x.login('dbcbot', 'dbcbot1')
+# response = x.get_subreddit("DBCTest")
+# parsed = JSON.parse(response.to_json)
+# ids = []
+# parsed['data']['children'].each do |thingy|
+#   if thingy['data']['title'].include? "dbctestpost"
+#     ids << thingy['data']['id']
+#   end
+# end
 
-ap ids
+# ap ids
